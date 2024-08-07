542f78e07d0697749d61517b0b9bcf79882178bf
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-07 15:36:14 +0300

    Add Exercise 1: Implement Array.prototype.map() to double values in an array

---
+++ exercises/exercises.js
@@ -0,0 +1,8 @@
+// Exercise 1: Applying Array.prototype.map()
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+const doubledNums = nums.map(num => num * 2);
+
+console.log(doubledNums); // [26, 174, 4, 178, 24, 8, 180, 126]
+
