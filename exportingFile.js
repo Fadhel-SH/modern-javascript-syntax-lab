d0f090ec90243e7a0cb13a6299081f877636e246
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-07 15:55:11 +0300

    Add Exercise 7: Implement import/export with ES6 modules in Node.js project

---
+++ exportingFile.js
@@ -0,0 +1,4 @@
+// exportingFile.js
+export default 'Matt';
+export const age = 43;
+export const job = 'programmer';
---
+++ importingFile.js
@@ -0,0 +1,3 @@
+// importingFile.js
+import name, { age, job } from './exportingFile.js';
+console.log(name, age, job); // 'Matt', 43, 'programmer'
